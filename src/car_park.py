fce9e82c7f4be8c78689a90e5a147a262fe77b3e
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2025-06-26 21:31:09 +0800

    Add CarPark class with default values and initialization

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location = "Unknown", capacity = 100, plates = None, display = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.display = display or []
+
+    def __str__(self):
+        
