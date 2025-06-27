409a56d5c7cea8d57a1e2dc1625fa5effc1458b2
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2025-06-27 13:22:38 +0800

    added unit tests for the car park class

--- src/sensor.py
+++ src/sensor.py
@@ -1,4 +1,7 @@
-class Sensor:
+import random
+from abc import ABC, abstractmethod
+
+class Sensor(ABC):
     def __init__(self, Sensor_Id, Is_active, Car_park):
         self.Sensor_Id = Sensor_Id
         self.Is_active = Is_active
@@ -8,10 +11,33 @@ class Sensor:
         status = "Active" if self.Is_active else "Inactive"
         return f"Sensor {self.Sensor_Id} is {status}"
 
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+    def _scan_plate(self):
+        return 'Fake-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
 class EntrySensor(Sensor):
     def __init__(self, Sensor_Id, Is_active, Car_park):
         super().__init__(Sensor_Id, Is_active, Car_park)
 
+    def update_car_park(self, plate):
+        self.Car_park.add_car(plate)
+        print(f"incoming vehicle detect. Plate:  {plate}")
+
 class ExitSensor(Sensor):
     def __init__(self, Sensor_Id, Is_active, Car_park):
         super().__init__(Sensor_Id, Is_active, Car_park)
+
+    def update_car_park(self, plate):
+        self.Car_park.remove_car(plate)
+        print(f"outcoming vehicle detect. Plate:  {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.Car_park.plates)
+
